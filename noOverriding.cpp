14c7855e38983e033f2ebd0e0cb1f8582535b631
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-05-27 14:52:07 +0700

    class baseClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 
+class baseClass {
+    public:
+    virtual void perkenelan() final {
+        cout << "Hallo saya function dari base class";
+    }
+};
+
