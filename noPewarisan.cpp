347a7758836effcc8af47ce006982146012c9efc
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-05-27 14:59:22 +0700

    noPewarisan selesai

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class baseClass final {
+    public:
+    virtual void perkenelan()  {
+        cout << "Hallo saya function dari base class";
+    }
+};
+
+class derivedClass : public baseClass {
+    public:
+    void perkenalan() {
+        cout << "Hallo saya function dari derived class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+};
